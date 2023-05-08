809b2fd03e331467fbc6fa7b61bbcce8fdd57781
JDM <shchuka_pasha@mail.ru>
2023-05-08 17:34:58 +0300

    feat: add favorites page

--- job-search/src/pages/FavoritesPage/FavoritesPage.tsx
+++ job-search/src/pages/FavoritesPage/FavoritesPage.tsx
@@ -1,9 +1,23 @@
 import { FC } from 'react'
 
+import person from 'assets/icons/person.svg'
+
 import Layout from 'components/Layout'
 
+import styles from './FavoritesPage.module.scss'
+
 const FavoritesPage: FC = () => {
-  return <Layout>FavoritesPage</Layout>
+  return (
+    <Layout>
+      <div className={styles.wrapper}>
+        <div className={styles.block}>
+          <img className={styles.person} src={person} alt="person" />
+          <h2 className={styles.title}>Упс, здесь еще ничего нет!</h2>
+          <button className={styles.button}>Поиск Вакансий</button>
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export default FavoritesPage
