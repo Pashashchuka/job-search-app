c4aa70bcbcd412c841eea1e463948d37d2784afb
JDM <shchuka_pasha@mail.ru>
2023-05-14 17:57:13 +0300

    feat: add favorites page with fav vacancies

--- job-search/src/pages/FavoritesPage/FavoritesPage.tsx
+++ job-search/src/pages/FavoritesPage/FavoritesPage.tsx
@@ -1,7 +1,11 @@
 import { FC } from 'react'
 
-import person from 'assets/icons/person.svg'
+import { IVacancy } from 'api'
 
+import person from 'assets/icons/person.svg'
+import arrow from 'assets/icons/down.svg'
+
+import Vacancy from 'components/Vacancy'
 import Layout from 'components/Layout'
 import Button from 'components/Button'
 
@@ -11,19 +15,47 @@ import styles from './FavoritesPage.module.scss'
 
 const FavoritesPage: FC = () => {
   const { handleClick } = useFavoritesPage()
+  const favVacancies = JSON.parse(localStorage.getItem('favVacancies'))
 
   return (
     <Layout>
       <div className={styles.wrapper}>
-        <div className={styles.block}>
-          <img className={styles.person} src={person} alt="person" />
-          <h2 className={styles.title}>Упс, здесь еще ничего нет!</h2>
-          <Button
-            className={styles.button}
-            handleClick={handleClick}
-            content="Поиск Вакансий"
-          />
-        </div>
+        {favVacancies ? (
+          <div className={styles.vacancyBlock}>
+            {favVacancies.map((vacancy: IVacancy) => (
+              <Vacancy key={vacancy.id} vacancy={vacancy} />
+            ))}
+             <div className={styles.pagination}>
+                <button className={styles.arrowBtn}>
+                  <img
+                    className={styles.prevIcon}
+                    src={arrow}
+                    alt="arrowPrev"
+                  />
+                </button>
+                <button className={styles.pageBtn}>1</button>
+                <button className={styles.pageBtn}>2</button>
+                <button className={styles.pageBtn}>3</button>
+                <button className={styles.arrowBtn}>
+                  <img
+                    className={styles.nextIcon}
+                    src={arrow}
+                    alt="arrowNext"
+                  />
+                </button>
+              </div>
+          </div>
+        ) : (
+          <div className={styles.block}>
+            <img className={styles.person} src={person} alt="person" />
+            <h2 className={styles.title}>Упс, здесь еще ничего нет!</h2>
+            <Button
+              className={styles.button}
+              handleClick={handleClick}
+              content="Поиск Вакансий"
+            />
+          </div>
+        )}
       </div>
     </Layout>
   )
