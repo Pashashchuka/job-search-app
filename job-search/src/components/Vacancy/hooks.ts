e08d18e3c033067980c557af85920b21abaa05ae
JDM <shchuka_pasha@mail.ru>
2023-05-14 17:45:03 +0300

    feat: add ability to choose fav vacancy

--- job-search/src/components/Vacancy/hooks.ts
+++ job-search/src/components/Vacancy/hooks.ts
@@ -6,6 +6,10 @@ export const useVacancy = () => {
   const [isFillStar, setIsFillStar] = useState<boolean>(false)
 
   const handleStarClick = (vacancy: IVacancy) => {
+    const favVacancies = JSON.parse(localStorage.getItem('favVacancies')) || []
+
+    favVacancies.push(vacancy)
+    localStorage.setItem('favVacancies', JSON.stringify(favVacancies))
     setIsFillStar(true)
   }
 
